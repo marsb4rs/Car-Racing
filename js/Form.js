b944a8947cb3822b719fb8aa79893d9cde28788a
marsb4rs <87779733+marsb4rs@users.noreply.github.com>
2021-09-19 12:38:42 -0400

    Add files via upload

--- js/Form.js
+++ js/Form.js
@@ -7,7 +7,7 @@ class Form {
   }
 
   setElementsPosition() {
-    this.titleImg.position(120,50);
+    this.titleImg.position(120, 50);
     this.input.position(width / 2 - 110, height / 2 - 80);
     this.playButton.position(width / 2 - 90, height / 2 - 20);
     this.greeting.position(width / 2 - 300, height / 2 - 100);
@@ -34,6 +34,12 @@ class Form {
       Hello ${this.input.value()}
       </br>wait for another player to join...`;
       this.greeting.html(message);
+      playerCount += 1;
+      player.name = this.input.value();
+      player.index = playerCount;
+      player.addPlayer();
+      player.updateCount(playerCount);
+      player.getDistance();
     });
   }
 
